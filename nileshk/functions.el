e41ad67c81c42cfcd6f7b45e82be08c97871ca0b
Nilesh Kapadia <code@nileshk.com>
2010-09-19 23:31:28 -0400

    Auto reload dired buffers when switching to them

--- nileshk/functions.el
+++ nileshk/functions.el
@@ -123,3 +123,18 @@
         100)
        (set-frame-parameter nil 'alpha '(100 100))
      (set-frame-parameter nil 'alpha '(85 60))))
+
+;;; Auto-refresh dired
+;;; http://nflath.com/2009/07/dired/
+(defadvice switch-to-buffer-other-window (after auto-refresh-dired (buffer &optional norecord) activate)
+  (if (equal major-mode 'dired-mode)
+      (revert-buffer)))
+(defadvice switch-to-buffer (after auto-refresh-dired (buffer &optional norecord) activate)
+  (if (equal major-mode 'dired-mode)
+      (revert-buffer)))
+(defadvice display-buffer (after auto-refresh-dired (buffer &optional not-this-window frame)  activate)
+  (if (equal major-mode 'dired-mode)
+      (revert-buffer)))
+(defadvice other-window (after auto-refresh-dired (arg &optional all-frame) activate)
+  (if (equal major-mode 'dired-mode)
+      (revert-buffer)))
