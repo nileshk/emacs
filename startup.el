795d2dbaa9e5616a122ba6dbd75bc744360fbef0
Nilesh Kapadia <code@nileshk.com>
2016-02-02 11:34:39 -0500

    Commented out flymake config that I'm not using

--- startup.el
+++ startup.el
@@ -197,26 +197,26 @@
      (if window-system (require 'fixpath))))
 
 ;;; Flymake-Pylint
-(eval-after-load "flymake"
-  '(progn
-     (defun flymake-pylint-init ()
-       (if window-system (require 'fixpath))
-       (local-set-key (kbd "C-.") 'flymake-goto-next-error-with-display)
-       (local-set-key (kbd "C->") 'flymake-goto-prev-error-with-display)
-       (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                          'flymake-create-temp-inplace))
-              (local-file (file-relative-name
-                           temp-file
-                           (file-name-directory buffer-file-name))))
-         (list (concat emacs-root-p "support/epylint2.py") (list local-file))))
+;; (eval-after-load "flymake"
+;;   '(progn
+;;      (defun flymake-pylint-init ()
+;;        (if window-system (require 'fixpath))
+;;        (local-set-key (kbd "C-.") 'flymake-goto-next-error-with-display)
+;;        (local-set-key (kbd "C->") 'flymake-goto-prev-error-with-display)
+;;        (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                           'flymake-create-temp-inplace))
+;;               (local-file (file-relative-name
+;;                            temp-file
+;;                            (file-name-directory buffer-file-name))))
+;;          (list (concat emacs-root-p "support/epylint2.py") (list local-file))))
      
-     (add-to-list 'flymake-allowed-file-name-masks
-                  '("\\.py\\'" flymake-pylint-init))))
+;;      (add-to-list 'flymake-allowed-file-name-masks
+;;                   '("\\.py\\'" flymake-pylint-init))))
 
-(add-hook 'python-mode-hook
-          '(lambda ()
-             (flymake-mode t)
-             ))
+;; (add-hook 'python-mode-hook
+;;           '(lambda ()
+;;              (flymake-mode t)
+;;              ))
 
 ;;; XML
 (add-to-list 'auto-mode-alist
